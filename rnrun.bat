38ba66c492ceee00d9317ca896f00e4fa013ad6e
ishaquehassan <ishaquehassan@gmail.com>
2017-09-04 12:56:56 +0500

    Added Apk folder opening shortcut

--- rnrun.bat
+++ rnrun.bat
@@ -13,7 +13,15 @@ cd android && gradlew clean && cd ..
 ) 
 
 IF "%1" == "build-release" (
-cd android && gradlew clean && gradlew assembleRelease && cd ..
+cd android && gradlew clean && gradlew assembleRelease
+%SystemRoot%\explorer.exe "%pathString%\android\app\build\outputs\apk"
+cd ..
 )
 
+IF "%1" == "show-apks" (
+%SystemRoot%\explorer.exe "%pathString%\android\app\build\outputs\apk"
+)
+
+IF NOT "%2" == "not-run" (
 react-native run-android
+)
